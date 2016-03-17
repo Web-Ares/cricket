d3f0ad4b58a5b080fd1322974d708a3efb83aa2f
snike1985 <snike1985@gmail.com>
2016-03-17 14:15:18 +0200

    created new page

--- app/js/jquery.main.js
+++ app/js/jquery.main.js
@@ -1,4 +1,31 @@
-"use strict";
-( function(){
+$( function() {
 
-} )();
+    'use strict';
+    $( function() {
+
+        $( '.menu' ).each( function() {
+            new Menu( $( this ) );
+        } );
+
+    } );
+
+    var Menu = function( obj ) {
+
+        var _obj = obj,
+            _btn = _obj.find( '.menu__btn' );
+
+        var _addEvents = function() {
+                _btn.on( {
+                    click: function() {
+                        _obj.toggleClass( 'menu_open' );
+                    }
+                } );
+            },
+            _init = function() {
+                _addEvents();
+            };
+
+        _init();
+    };
+
+} );
